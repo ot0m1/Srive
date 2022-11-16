5c1f8ad5a2e5eed9da2eb5211bcfafb317a2f0c8
ot0m1 <coding_addiction@yahoo.co.jp>
2022-11-16 19:48:29 +0900

    add [...nextauth].ts

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,73 @@
+import NextAuth from 'next-auth'
+import SpotifyProvider from 'next-auth/providers/spotify'
+
+async function refreshAccessToken(token: any) {
+  try {
+    const url =
+      'https://accounts.spotify.com/api/token?' +
+      new URLSearchParams({
+        client_id: process.env.CLIENT_ID || '',
+        client_secret: process.env.CLIENT_SECRET || '',
+        grant_type: 'refresh_token',
+        refresh_token: token.refreshToken,
+      })
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      method: 'POST',
+    })
+
+    const refreshedTokens = await response.json()
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken,
+    };
+  } catch (error) {
+    console.log(error)
+
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    };
+  }
+}
+
+export const authOptions = {
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.CLIENT_ID || '',
+      clientSecret: process.env.CLIENT_SECRET || '',
+      authorization: {
+        url: 'https://accounts.spotify.com/authorize',
+        params: {scope: 'playlist-modify-private,playlist-read-private,playlist-read-collaborative'}
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.id = account.id
+        token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+      }
+
+      return Date.now() < token.accessTokenExpires ? token : refreshAccessToken(token)
+    },
+    async session({ session, token }) {
+      session.token = token
+
+      return session
+    },
+  },
+}
+
+export default NextAuth(authOptions)
