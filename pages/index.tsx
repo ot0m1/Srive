885797d31883144a9886339d587ad435989bb5c0
ot0m1 <coding_addiction@yahoo.co.jp>
2022-11-30 20:41:53 +0900

    adjust index

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,6 @@ import React from 'react'
 import { useSession, signOut } from 'next-auth/react'
 
 const Home: NextPage = () => {
-  const { data: session, status } = useSession()
-
-  const isLoggedIn = (status: string) => {
-    return status === 'authenticated' ? true : false
-  }
-
   return (
     <div className="container mx-auto md:w-[60%] flex flex-col min-h-screen">
       <Header />
